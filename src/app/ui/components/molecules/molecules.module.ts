6983081ce8d6ba25836ea7def9ae270a1f9a3106
David Diaz Montenegro <davidcol123dm@gmail.com>
2025-05-04 01:23:21 -0500

    hace el post y el validacion asincrona falta test, componetisar y mensaje de succes

--- src/app/ui/components/molecules/molecules.module.ts
+++ src/app/ui/components/molecules/molecules.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NotificationComponent } from './notification/notification.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+
+    NotificationComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    NotificationComponent
   ]
 })
 export class MoleculesModule { }
